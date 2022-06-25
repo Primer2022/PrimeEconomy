67ab5924dadd01a55de3461a675cbce89f33ea29
Primer <dovydov.p@yandex.ru>
2022-06-26 03:42:47 +1000

    initial commit

--- src/main/java/ru/primer/mc/Main.java
+++ src/main/java/ru/primer/mc/Main.java
@@ -3,7 +3,6 @@ package ru.primer.mc;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.Nullable;
 import ru.primer.mc.Commands.CommandEco;
 import ru.primer.mc.Commands.CommandMoney;
 import ru.primer.mc.Commands.CommandPay;
@@ -20,13 +19,16 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
         instance = this;
         saveDefaultConfig();
+
         if (!setupEconomy() ) {
             log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
+
         getCommand("money").setExecutor(new CommandMoney());
         getCommand("economy").setExecutor(new CommandEco());
         getCommand("pay").setExecutor(new CommandPay());
@@ -38,13 +40,17 @@ public final class Main extends JavaPlugin {
     }
 
     private boolean setupEconomy() {
+
         if (getServer().getPluginManager().getPlugin("Vault") == null) {
             return false;
         }
-        @Nullable RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+
         if (rsp == null) {
             return false;
         }
+
         economy = rsp.getProvider();
         return economy != null;
     }
