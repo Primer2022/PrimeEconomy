6014217603c157aff1eb42200bd1754d3da51470
Primer <dovydov.p@yandex.ru>
2022-06-26 04:52:58 +1000

    initial commit

--- src/main/java/ru/primer/mc/Utils.java
+++ src/main/java/ru/primer/mc/Utils.java
@@ -8,6 +8,7 @@ public class Utils {
     public static void message(String message, Player p) {
         p.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
     }
+
     public static void messageNotPlayer(FileConfiguration cfg) {
         System.out.println(cfg.getString("not-player").replace('&', '§'));
     }
